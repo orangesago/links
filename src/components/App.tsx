be6ac9c48cc485ebffea81db6e286d22fc5beec5
Hsi <its.hsichen@gmail.com>
2026-08-16 23:29:16 +0800

    fix: update twitter profile link

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -50,7 +50,7 @@ const socialLinks: readonly SocialLink[] = [
     },
     {
         label: 'Twitter',
-        href: 'https://x.com/OrangeSago',
+        href: 'https://x.com/sagocream_',
         logoAlt: 'Twitter',
         logoSrc: '/svgl/x.svg',
         toneClass: 'social-link--twitter',
